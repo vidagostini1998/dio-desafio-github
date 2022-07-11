96bc020519cfd0ddc1f4e9a00a3c71427d0d71a9
vidagostini1998 <vinicius.hd.ti@gmail.com>
2022-07-11 10:44:12 -0300

    Atividade 2

---
+++ JavaScript/atividade 2.js
@@ -0,0 +1,9 @@
+var firstName = "Vinicius";
+var lastName = "DAgostini";
+
+if(firstName === "Vinicius"){
+    var firstName = "Pedro";
+    let lastName = "Silva";
+}
+
+console.log(firstName, lastName);

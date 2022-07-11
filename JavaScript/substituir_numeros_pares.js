eb77c0be3dbe60dbe14a146f55a0f8ffc58a01fc
vidagostini1998 <vinicius.hd.ti@gmail.com>
2022-07-11 12:29:38 -0300

    Atividade substituir numeros pares

---
+++ JavaScript/substituir_numeros_pares.js
@@ -0,0 +1,14 @@
+function substituiPares(array){
+    if(!array.length) return -1;
+
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === 0){
+            console.log("Você já é zero!!");
+        }else if(array[i] % 2 === 0){
+            console.log(`Substituindo ${array[i]} por 0...`);
+            array[i] = 0;
+        }
+    }
+    console.log(array);
+}
+substituiPares([1,3,4,6,80,33,23,90]);

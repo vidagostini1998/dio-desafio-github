525682fb9887778f13585b7e86a343d30bc42199
vidagostini1998 <vinicius.hd.ti@gmail.com>
2022-07-12 11:31:33 -0300

    Atividade Modulos JS

---
+++ JavaScript/Atividade modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade,mostraIdade,mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Vinicius",23));
+console.log(mostraCidade("Vinicius","Americana"));
+console.log(mostraHobby("Vinicius","Jogar"));
